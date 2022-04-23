ce4641cc6e73e0cd2d536744a1610153b655310a
Lauri Saar <lauri.saar@outlook.com>
2022-04-23 13:34:11 +0100

    CHANGE: 2.10 Displaying a List of Elements in Vue 3

---
+++ src/components/PageHome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>Hello from Page Home</div>
+  <div v-for="t in threads" :key="t.id">
+    <h2 v-text="t.title" />
+    <div v-for="postId in t.posts" :key="postId">
+      <p v-text="userById(postById(postId).userId).name" />
+      <p v-text="postById(postId).text" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { threads, posts, users } from "../data.json";
+
+function postById(pId) {
+  return posts.find(({ id }) => id === pId);
+}
+
+function userById(uId) {
+  return users.find(({ id }) => id === uId);
+}
+</script>
+
+<style scoped></style>
