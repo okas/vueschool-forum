d0ef428e8aca015250ba3b706ca9d5c9acf974fd
Lauri Saar <lauri.saar@outlook.com>
2022-04-24 18:00:43 +0100

    CHANGE: 3.18 Creating a Not Found Page with Vue Router:
    - soft 404!

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="col-full text-center">
+    <h1>(soft 404) Not Found 😒</h1>
+    <router-link :to="{ name: `Home` }">Read some cool threads</router-link>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,12 @@ const routes = [
     component: () => import("../components/PageThreadShow.vue"),
     props: true,
   },
+  {
+    // will match everything and put it under `$route.params.pathMatch`
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("../components/PageNotFound.vue"),
+  },
 ];
 
 export default createRouter({ history: createWebHistory(), routes });
