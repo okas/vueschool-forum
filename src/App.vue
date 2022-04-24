5f99c9435fa0c807380c57511822f17d7d5467c1
Lauri Saar <lauri.saar@outlook.com>
2022-04-24 13:32:56 +0100

    CHANGE: 3.15 Extract Router Configuration:
    -NPM, installed plugin @vue/cli-plugin-router,
    - moved over current code;
    - customized plugin installation, by retain some of scaffold code.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
   <img alt="Vue logo" src="./assets/logo.png" />
   <div class="container">
     <router-view />
@@ -19,4 +23,17 @@
   color: #2c3e50;
   margin-top: 60px;
 }
+
+/* nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+} */
 </style>
