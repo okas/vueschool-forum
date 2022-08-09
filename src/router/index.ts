00bd14ebd2cc5ad169ddf6c7f90f0c17cf11d5a0
Lauri Saar <lauri.saar@outlook.com>
2022-08-10 00:04:10 +0100

    REFACTOR: routes options typing & simplification

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import {
   createRouter,
   createWebHistory,
-  RouteLocationNormalized,
+  RouteLocation,
+  RouteLocationRaw,
+  RouteRecordRaw,
 } from "vue-router";
 import { threads as threadData } from "../data.json";
 
-const routes = [
+const routes: Readonly<RouteRecordRaw[]> = [
   {
     path: "/",
     name: "Home",
@@ -17,22 +19,10 @@ const routes = [
     component: () => import("../pages/PageThreadShow.vue"),
     props: true,
     beforeEnter: (
-      { params, path, query, hash }: RouteLocationNormalized,
-      _: undefined,
-      next
-    ) =>
-      next(
-        threadData.some(({ id }) => id === params.id)
-          ? undefined
-          : {
-              name: "NotFound",
-              params: {
-                pathMatch: path.substring(1).split("/"),
-              },
-              query,
-              hash,
-            }
-      ),
+      routeObj,
+      _: unknown,
+      next: (p: RouteLocationRaw | undefined) => void
+    ) => next(test(routeObj)),
   },
   {
     // will match everything and put it under `$route.params.pathMatch`
@@ -41,5 +31,21 @@ const routes = [
     component: () => import("../pages/PageNotFound.vue"),
   },
 ];
+
+function test({
+  params,
+  path,
+  query,
+  hash,
+}: RouteLocation): RouteLocationRaw | undefined {
+  return threadData.some(({ id }) => id === params.id)
+    ? undefined
+    : {
+        name: "NotFound",
+        params: { pathMatch: path.substring(1).split("/") },
+        query,
+        hash,
+      };
+}
 
 export default createRouter({ history: createWebHistory(), routes });
