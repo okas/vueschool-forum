76b9e8efde86177b7ef10794efcde12f99a6119d
Lauri Saar <lauri.saar@outlook.com>
2022-08-09 22:51:16 +0100

    REFACTOR: global module auto loader to plugin

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,19 @@
 import { createApp } from "vue";
+import AppRoot from "./App.vue";
 import router from "./router";
-import App from "./App.vue";
+import registerGlobalComponents from "./utils/useAutoComponentRegistrator";
 
-const forumApp = createApp(App);
+const forumApp = createApp(AppRoot);
 
 forumApp.use(router);
-registerGlobalComponents(forumApp);
+
+forumApp.use(registerGlobalComponents, {
+  loader: () =>
+    // Webpack specific!
+    // In case of Vite/Rollup there is builtin glob importer.
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    require.context("@/components/", true, /App[A-Z]\w+\.(vue|js)$/),
+});
 
 forumApp.mount("#app");
-
-/********************/
-
-function registerGlobalComponents(app) {
-  // Webpack specific!
-  // In case of Vite/Rollup there is builtin glob importer.
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const requireComponent = require.context(
-    "./components/",
-    true,
-    /App[A-Z]\w+\.(vue|js)$/
-  );
-
-  requireComponent.keys().forEach((fileName: string) => {
-    let baseComponentConfig = requireComponent(fileName);
-    baseComponentConfig = baseComponentConfig.default || baseComponentConfig;
-
-    const baseComponentName =
-      baseComponentConfig.name ||
-      fileName.replace(/^.+\//, "").replace(/\.\w+$/, "");
-
-    app.component(baseComponentName, baseComponentConfig);
-  });
-}
