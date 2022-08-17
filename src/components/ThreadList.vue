4ed54979850cf31c5c440349c502b5f3b2f58371
Lauri Saar <lauri.saar@outlook.com>
2022-08-17 16:40:25 +0100

    FIX: removed unneeded structural div

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -32,61 +32,59 @@ function countPhrase(count: number) {
 </script>
 
 <template>
-  <div class="col-full">
-    <div class="thread-list">
-      <h2 class="list-title">Threads</h2>
+  <div class="thread-list">
+    <h2 class="list-title">Threads</h2>
 
-      <div
-        v-for="{
-          threadId,
-          title,
-          publishedAt,
-          repliesCount,
-          userName,
-          userAvatar,
-        } of renderData"
-        :key="threadId"
-        class="thread"
-      >
+    <div
+      v-for="{
+        threadId,
+        title,
+        publishedAt,
+        repliesCount,
+        userName,
+        userAvatar,
+      } of renderData"
+      :key="threadId"
+      class="thread"
+    >
+      <div>
+        <p>
+          <router-link :to="{ name: `ThreadShow`, params: { threadId } }">
+            {{ title }}
+          </router-link>
+        </p>
+        <p class="text-faded text-xsmall">
+          By <a href="#" v-text="userName" />,
+          <app-date :timestamp="publishedAt" />.
+          <!-- By <a href="profile.html" v-text="userById(userId).name" />, {{ publishedAt }}. -->
+        </p>
+      </div>
+
+      <div class="activity">
+        <p class="replies-count" v-text="countPhrase(repliesCount)" />
+
+        <img class="avatar-medium" :src="userAvatar" alt="" />
+
         <div>
-          <p>
-            <router-link :to="{ name: `ThreadShow`, params: { threadId } }">
-              {{ title }}
-            </router-link>
+          <p class="text-xsmall">
+            <a href="#" v-text="userName" />
+            <!-- <a href="profile.html" v-text="userById(userId)" /> -->
           </p>
-          <p class="text-faded text-xsmall">
-            By <a href="#" v-text="userName" />,
-            <app-date :timestamp="publishedAt" />.
-            <!-- By <a href="profile.html" v-text="userById(userId).name" />, {{ publishedAt }}. -->
+          <p class="text-xsmall text-faded">
+            <app-date :timestamp="publishedAt" />
           </p>
-        </div>
-
-        <div class="activity">
-          <p class="replies-count" v-text="countPhrase(repliesCount)" />
-
-          <img class="avatar-medium" :src="userAvatar" alt="" />
-
-          <div>
-            <p class="text-xsmall">
-              <a href="#" v-text="userName" />
-              <!-- <a href="profile.html" v-text="userById(userId)" /> -->
-            </p>
-            <p class="text-xsmall text-faded">
-              <app-date :timestamp="publishedAt" />
-            </p>
-          </div>
         </div>
       </div>
     </div>
-
-    <!-- <div class="pagination"></div>
-      <button class="btn-circle" disabled="">
-        <i class="fa fa-angle-left"></i>
-      </button>
-      1 of 3
-      <button class="btn-circle"><i class="fa fa-angle-right"></i></button>
-    </div> -->
   </div>
+
+  <!-- <div class="pagination"></div>
+    <button class="btn-circle" disabled="">
+      <i class="fa fa-angle-left"></i>
+    </button>
+    1 of 3
+    <button class="btn-circle"><i class="fa fa-angle-right"></i></button>
+  </div> -->
 </template>
 
 <style scoped></style>
