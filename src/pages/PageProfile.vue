4ef29be55f9e1c9493aa80c2891bd8c81fd629e0
Lauri Saar <lauri.saar@outlook.com>
2022-08-23 22:42:48 +0100

    CHANGE: profile page's explicit fetching was missing

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -11,7 +11,12 @@ defineProps<{
   edit?: boolean;
 }>();
 
-const { getAuthUser } = storeToRefs(useMainStore());
+const store = useMainStore();
+// < FETCH
+store.fetchAuthUser();
+// > FETCH
+
+const { getAuthUser } = storeToRefs(store);
 </script>
 
 <template>
