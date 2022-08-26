1d5283090000a7d2ac5082cf7eb85bfe7467c259
Lauri Saar <lauri.saar@outlook.com>
2022-08-26 16:48:24 +0100

    CHANGE: 9.75 Hiding Content Until Data is Fully Loaded: - one page was left behind

--- src/pages/PageCategory.vue
+++ src/pages/PageCategory.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useAsyncState } from "@vueuse/core";
 import { computed } from "vue";
 import CategoryListItem from "../components/CategoryListItem.vue";
 import { useMainStore } from "../stores/main-store";
@@ -8,9 +9,12 @@ const props = defineProps<{
 }>();
 
 const store = useMainStore();
-// < FETCH
-await store.fetchForums((await store.fetchCategory(props.categoryId)).forums);
-// > FETCH
+
+const { isReady } = useAsyncState(async () => {
+  const category = await store.fetchCategory(props.categoryId);
+  await store.fetchForums(category.forums);
+}, undefined);
+
 const name = computed(() => store.getCategoryNamedFn(props.categoryId));
 
 const categoryForums = computed(() =>
@@ -19,12 +23,16 @@ const categoryForums = computed(() =>
 </script>
 
 <template>
-  <div class="col-full push-top">
-    <h1 v-text="name" />
-  </div>
-  <div class="col-full">
-    <category-list-item v-bind="{ categoryId, name, forums: categoryForums }" />
-  </div>
+  <template v-if="isReady">
+    <div class="col-full push-top">
+      <h1 v-text="name" />
+    </div>
+    <div class="col-full">
+      <category-list-item
+        v-bind="{ categoryId, name, forums: categoryForums }"
+      />
+    </div>
+  </template>
 </template>
 
 <style scoped></style>
