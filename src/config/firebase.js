c01c199e34106424929098ed88e6fd3a1061e8ec
Lauri Saar <lauri.saar@outlook.com>
2022-08-27 08:22:45 +0100

    CHANGE: 10.83 Using .env for Secure Secrets: - migrated non-secret env.vars to `.env` file, as they are required to run the client; - vars rename along the way.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 export default {
-  apiKey: process.env.VUE_APP_apiKey,
-  authDomain: process.env.VUE_APP_authDomain,
-  projectId: process.env.VUE_APP_projectId,
-  storageBucket: process.env.VUE_APP_storageBucket,
-  messagingSenderId: process.env.VUE_APP_messagingSenderId,
-  appId: process.env.VUE_APP_appId,
+  apiKey: process.env.VUE_APP_FIREBASE_apiKey,
+  authDomain: process.env.VUE_APP_FIREBASE_authDomain,
+  projectId: process.env.VUE_APP_FIREBASE_projectId,
+  storageBucket: process.env.VUE_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_messagingSenderId,
+  appId: process.env.VUE_APP_FIREBASE_appId,
 };
