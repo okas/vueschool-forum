b7625738936ba09013a39e811493358eb2d31e9a
Lauri Saar <lauri.saar@outlook.com>
2022-08-28 13:54:07 +0100

    CHANGE(router): apply meta to all routes & code breakdown

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,140 +1,18 @@
 import {
   createRouter,
   createWebHistory,
-  RouteLocation,
   RouteLocationNormalized,
-  RouteLocationRaw,
   RouteRecordRaw,
 } from "vue-router";
-import { useMainStore } from "../stores/main-store";
-import { HasId } from "../types/HasId";
+import { rawRoutes } from "./raw-routes";
 
-const routes: Readonly<RouteRecordRaw[]> = [
-  {
-    path: "/",
-    name: "Home",
-    component: () => import("../pages/PageHome.vue"),
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: () => import("../pages/PageRegister.vue"),
-  },
-  {
-    path: "/me",
-    name: "Profile",
-    component: () => import("../pages/PageProfile.vue"),
-    meta: { toTop: true, smoothScroll: true },
-    beforeEnter: (
-      routeObj,
-      _: unknown,
-      next: (route: RouteLocationRaw | undefined) => void
-    ) =>
-      next(
-        testForNotFound(
-          routeObj,
-          useMainStore().users,
-          useMainStore().authUserId
-        )
-      ),
-  },
-  {
-    path: "/me/edit",
-    name: "ProfileEdit",
-    component: () => import("../pages/PageProfile.vue"),
-    props: { edit: true },
-    beforeEnter: (
-      routeObj,
-      _: unknown,
-      next: (route: RouteLocationRaw | undefined) => void
-    ) =>
-      next(
-        testForNotFound(
-          routeObj,
-          useMainStore().users,
-          useMainStore().authUserId
-        )
-      ),
-  },
-  {
-    path: "/category/:categoryId",
-    name: "Category",
-    component: () => import("../pages/PageCategory.vue"),
-    props: true,
-    beforeEnter: (
-      routeObj,
-      _: unknown,
-      next: (route: RouteLocationRaw | undefined) => void
-    ) =>
-      next(testForNotFound(routeObj, useMainStore().categories, "categoryId")),
-  },
-  {
-    path: "/forum/:forumId",
-    name: "Forum",
-    component: () => import("../pages/PageForum.vue"),
-    props: true,
-    beforeEnter: (
-      routeObj,
-      _: unknown,
-      next: (route: RouteLocationRaw | undefined) => void
-    ) => next(testForNotFound(routeObj, useMainStore().forums, "forumId")),
-  },
-  {
-    path: "/forum/:forumId/thread/create",
-    name: "ThreadCreate",
-    component: () => import("../pages/PageThreadCreate.vue"),
-    props: true,
-    beforeEnter: (
-      routeObj,
-      _: unknown,
-      next: (route: RouteLocationRaw | undefined) => void
-    ) => next(testForNotFound(routeObj, useMainStore().forums, "forumId")),
-  },
-  {
-    path: "/thread/:threadId/edit",
-    name: "ThreadEdit",
-    component: () => import("../pages/PageThreadEdit.vue"),
-    props: true,
-    beforeEnter: (
-      routeObj,
-      _: unknown,
-      next: (route: RouteLocationRaw | undefined) => void
-    ) => next(testForNotFound(routeObj, useMainStore().threads, "threadId")),
-  },
-  {
-    path: "/thread/:threadId",
-    name: "ThreadShow",
-    component: () => import("../pages/PageThread.vue"),
-    props: true,
-    beforeEnter: (
-      routeObj,
-      _: unknown,
-      next: (route: RouteLocationRaw | undefined) => void
-    ) => next(testForNotFound(routeObj, useMainStore().threads, "threadId")),
-  },
-  {
-    // will match everything and put it under `$route.params.pathMatch`
-    path: "/:pathMatch(.*)*",
-    name: "NotFound",
-    component: () => import("../pages/PageNotFound.vue"),
-  },
-];
-
-function testForNotFound<TViewModel extends HasId>(
-  { params, path, query, hash }: RouteLocation,
-  array: Array<TViewModel>,
-  paramKeyOrIdValue: string
-): RouteLocationRaw | undefined {
-  return array.some(
-    ({ id }) => id === (params[paramKeyOrIdValue] ?? paramKeyOrIdValue)
-  )
-    ? undefined
-    : {
-        name: "NotFound",
-        params: { pathMatch: path.substring(1).split("/") },
-        query,
-        hash,
-      };
+function getRouteRecords(
+  input: Readonly<RouteRecordRaw[]>
+): Readonly<RouteRecordRaw[]> {
+  return input.map((rawRoute) => {
+    rawRoute.meta = { toTop: true, smoothScroll: true };
+    return rawRoute;
+  });
 }
 
 function scrollBehavior(to: RouteLocationNormalized): Promise<ScrollToOptions> {
@@ -148,10 +26,8 @@ function scrollBehavior(to: RouteLocationNormalized): Promise<ScrollToOptions> {
   );
 }
 
-const router = createRouter({
-  routes,
+export default createRouter({
+  routes: getRouteRecords(rawRoutes),
   scrollBehavior,
   history: createWebHistory(),
 });
-
-export default router;
---
+++ src/router/raw-routes.ts
@@ -0,0 +1,130 @@
+import { RouteLocation, RouteLocationRaw, RouteRecordRaw } from "vue-router";
+import { useMainStore } from "../stores/main-store";
+import { HasId } from "../types/HasId";
+
+export const rawRoutes: Readonly<RouteRecordRaw[]> = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("../pages/PageHome.vue"),
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../pages/PageRegister.vue"),
+  },
+  {
+    path: "/me",
+    name: "Profile",
+    component: () => import("../pages/PageProfile.vue"),
+    beforeEnter: (
+      routeObj,
+      _: unknown,
+      next: (route: RouteLocationRaw | undefined) => void
+    ) =>
+      next(
+        testForNotFound(
+          routeObj,
+          useMainStore().users,
+          useMainStore().authUserId
+        )
+      ),
+  },
+  {
+    path: "/me/edit",
+    name: "ProfileEdit",
+    component: () => import("../pages/PageProfile.vue"),
+    props: { edit: true },
+    beforeEnter: (
+      routeObj,
+      _: unknown,
+      next: (route: RouteLocationRaw | undefined) => void
+    ) =>
+      next(
+        testForNotFound(
+          routeObj,
+          useMainStore().users,
+          useMainStore().authUserId
+        )
+      ),
+  },
+  {
+    path: "/category/:categoryId",
+    name: "Category",
+    component: () => import("../pages/PageCategory.vue"),
+    props: true,
+    beforeEnter: (
+      routeObj,
+      _: unknown,
+      next: (route: RouteLocationRaw | undefined) => void
+    ) =>
+      next(testForNotFound(routeObj, useMainStore().categories, "categoryId")),
+  },
+  {
+    path: "/forum/:forumId",
+    name: "Forum",
+    component: () => import("../pages/PageForum.vue"),
+    props: true,
+    beforeEnter: (
+      routeObj,
+      _: unknown,
+      next: (route: RouteLocationRaw | undefined) => void
+    ) => next(testForNotFound(routeObj, useMainStore().forums, "forumId")),
+  },
+  {
+    path: "/forum/:forumId/thread/create",
+    name: "ThreadCreate",
+    component: () => import("../pages/PageThreadCreate.vue"),
+    props: true,
+    beforeEnter: (
+      routeObj,
+      _: unknown,
+      next: (route: RouteLocationRaw | undefined) => void
+    ) => next(testForNotFound(routeObj, useMainStore().forums, "forumId")),
+  },
+  {
+    path: "/thread/:threadId/edit",
+    name: "ThreadEdit",
+    component: () => import("../pages/PageThreadEdit.vue"),
+    props: true,
+    beforeEnter: (
+      routeObj,
+      _: unknown,
+      next: (route: RouteLocationRaw | undefined) => void
+    ) => next(testForNotFound(routeObj, useMainStore().threads, "threadId")),
+  },
+  {
+    path: "/thread/:threadId",
+    name: "ThreadShow",
+    component: () => import("../pages/PageThread.vue"),
+    props: true,
+    beforeEnter: (
+      routeObj,
+      _: unknown,
+      next: (route: RouteLocationRaw | undefined) => void
+    ) => next(testForNotFound(routeObj, useMainStore().threads, "threadId")),
+  },
+  {
+    // will match everything and put it under `$route.params.pathMatch`
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: () => import("../pages/PageNotFound.vue"),
+  },
+];
+
+function testForNotFound<TViewModel extends HasId>(
+  { params, path, query, hash }: RouteLocation,
+  array: Array<TViewModel>,
+  paramKeyOrIdValue: string
+): RouteLocationRaw | undefined {
+  return array.some(
+    ({ id }) => id === (params[paramKeyOrIdValue] ?? paramKeyOrIdValue)
+  )
+    ? undefined
+    : {
+        name: "NotFound",
+        params: { pathMatch: path.substring(1).split("/") },
+        query,
+        hash,
+      };
+}
