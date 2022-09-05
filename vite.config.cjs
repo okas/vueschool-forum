a716eb5d683841651c0cdf343099c8f471af4450
Lauri Saar <lauri.saar@outlook.com>
2022-09-05 19:26:16 +0100

    CHANGE(vue-ts): basic working dev server

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,11 +1,16 @@
+import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 8080,
   },
 });
