e647e1d4f55cdbdc0eb86abcc65e152476043012
Lauri Saar <lauri.saar@outlook.com>
2022-09-05 16:05:00 +0100

    FIX(vite conf): alias resolve logic was inappropriate

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,10 +1,11 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": import.meta.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });
