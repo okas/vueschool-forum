cb9248a1ee06e74ef624df252bc1d702871e6b91
Lauri Saar <lauri.saar@outlook.com>
2022-10-04 12:49:05 +0100

    CHANGE(16.118): Using the Transition Group Component

--- src/components/AppNotifications.vue
+++ src/components/AppNotifications.vue
@@ -5,7 +5,7 @@ const { notifications, removeNotification } = useNotifications();
 </script>
 
 <template>
-  <ul v-if="notifications?.size" class="notifications">
+  <transition-group name="notification" tag="ul" class="notifications-container">
     <li v-for="[id, message] of notifications" :key="id" class="notification">
       <span v-text="message" />
 
@@ -13,27 +13,42 @@ const { notifications, removeNotification } = useNotifications();
         <fa icon="xmark" />
       </button>
     </li>
-  </ul>
+  </transition-group>
 </template>
 
 <style scoped lang="scss">
-.notifications {
+.notifications-container {
   position: fixed;
   bottom: 1.5rem;
   right: 0.5rem;
+}
 
-  .notification {
-    background-color: white;
-    display: flex;
-    justify-content: space-between;
-    width: 20rem;
-    box-shadow: 2px 2px 3px 2px rgba(0, 0, 148, 0.5);
-    padding: 0.7rem 1.5rem;
-    border-left: 0.25rem solid #263959;
+.notification {
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  width: 20rem;
+  box-shadow: 2px 2px 3px 2px rgba(0, 0, 148, 0.5);
+  padding: 0.7rem 1.5rem;
+  border-left: 0.25rem solid #263959;
+
+  &:not(:last-of-type) {
+    margin-bottom: 0.5rem;
   }
 
-  .notification:not(:last-of-type) {
-    margin-bottom: 0.5rem;
+  &-enter-active,
+  &-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  &-enter-from,
+  &-leave-to {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+
+  &-move {
+    transition: transform 0.8s ease;
   }
 }
 </style>
