okas/vueschool-forum

commit 2db07aca346efb126bd1b4cb4ce0c5a77ec25abf

CHANGE(17.121): Editing the User Avatar on the Profile Page:
- Added type for avatar file handling.
- Added store action to upload and and update `UserVm.avatar`  in collection.
- Profile card and profile card editor, both support file.

- Refactoring along the way.

src/stores/user-store.ts
@@ -31,12 +31,14 @@ import {
   UserStoreState,
 } from "../types/user-store-types";
 import {
+  UserVMEditAvatarFile,
   UserVmEditForInput,
   UserVMNewFormInput,
   UserVMRegWithEmailAndPassword,
   UserVMWithActivity,
 } from "../types/userVm-types";
 import { findById } from "../utils/array-helpers";
+import { ok } from "../utils/assert-helpers";
 import { FirebaseSubscriptionManager } from "../utils/FirebaseSubscriptionManager";
 import {
   makeFirebaseFetchMultiDocsFn,
@@ -141,42 +143,24 @@ export const useUserStore = defineStore(
     async function registerUserWithEmailAndPassword({
       email,
       password,
-      avatar,
-      avatarFile,
       ...rest
     }: UserVMRegWithEmailAndPassword) {
       const {
         user: { uid },
       } = await createUserWithEmailAndPassword(fabAuth, email, password);
 
-      avatarFile && (avatar = await _overrideAvatarFromFile(avatarFile, uid));
-
       return await createUser(uid, {
         email,
-        avatar,
         ...rest,
       });
     }
 
-    async function _overrideAvatarFromFile(
-      fileObj: File,
-      uid: string
-    ): Promise<string> {
-      const storeFileName = `uploads/${uid}/images/${Date.now()}-${
-        fileObj.name
-      }`;
-
-      const storageRef = fabStoreRef(fabStor, storeFileName);
-
-      const { ref } = await uploadBytes(storageRef, fileObj);
-
-      return await getDownloadURL(ref);
-    }
-
     async function createUser(
       id: string,
-      { email, avatar = null, ...rest }: UserVMNewFormInput
+      { email, avatar = null, avatarFile, ...rest }: UserVMNewFormInput
     ): Promise<string> {
+      avatarFile && (avatar = await _uploadAvatar(avatarFile, id));
+
       const userDto: UserVMNewFormInput & {
         registeredAt: FieldValue;
       } & Pick<UserVM, "usernameLower" | "postsCount" | "threadsCount"> = {
@@ -195,9 +179,11 @@ export const useUserStore = defineStore(
     }
 
     async function editUser(
-      { id, ...rest }: UserVmEditForInput,
+      { id, avatarFile, ...rest }: UserVmEditForInput,
       fetchAfter = false
     ) {
+      avatarFile && (rest.avatar = await _uploadAvatar(avatarFile, id));
+
       const editDto = Object.fromEntries(
         Object.entries(rest).map(([k, v]) => [k, v === undefined ? null : v])
       );
@@ -207,6 +193,22 @@ export const useUserStore = defineStore(
       await updateDoc(userRef, editDto);
 
       fetchAfter && (await fetchUser(id));
+    }
+
+    async function updateAvatar({
+      id,
+      avatarFile,
+    }: UserVMEditAvatarFile): Promise<string> {
+      ok(avatarFile, "Avatar file update error: no file data.");
+
+      const avatar = await _uploadAvatar(avatarFile, id);
+
+      const userRef = _getRef(id);
+
+      await updateDoc(userRef, { avatar });
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return avatar!;
     }
 
     const fetchUser = makeFirebaseFetchSingleDocFn(
@@ -262,6 +264,16 @@ export const useUserStore = defineStore(
       return doc(fabDb, FabCollection.users, id);
     }
 
+    async function _uploadAvatar(fileObj: File, uid: string): Promise<string> {
+      const storeFileName = `uploads/${uid}/images/${fileObj.name}`;
+
+      const storageRef = fabStoreRef(fabStor, storeFileName);
+
+      const { ref } = await uploadBytes(storageRef, fileObj);
+
+      return getDownloadURL(ref);
+    }
+
     return {
       authUserId,
       items,
@@ -274,6 +286,7 @@ export const useUserStore = defineStore(
       registerUserWithEmailAndPassword,
       createUser,
       editUser,
+      updateAvatar,
       fetchUser,
       fetchUsers,
       fetchAuthUser,

src/components/ProfileCard.vue
@@ -1,41 +1,81 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import { UserVMWithActivity } from "../types/userVm-types";
-import {
-  diffFromUnix,
-  formatMonthYearFromUnix,
-} from "../utils/dateTimeDiffFormat";
+import { computedAsync, useFileDialog, UseFileDialogOptions } from "@vueuse/core";
+import { computed, watch } from "vue";
+import { UserVMEditAvatarFile, UserVMWithActivity } from "../types/userVm-types";
+import { diffFromUnix, formatMonthYearFromUnix } from "../utils/dateTimeDiffFormat";
 import { getCountPhrase } from "../utils/misc";
+
+const fileDialogOptions: UseFileDialogOptions = {
+  multiple: false,
+  accept: "image/*",
+};
 
 const props = defineProps<{
   authUser: UserVMWithActivity;
 }>();
 
-const memberSince = computed(() =>
-  formatMonthYearFromUnix(props.authUser.registeredAt)
+const emits = defineEmits<{
+  (e: "updateAvatarFile", dto: UserVMEditAvatarFile): void;
+}>();
+
+const { files, open, reset } = useFileDialog();
+
+const singleFile = computed(() => files.value?.item(0));
+
+const memberSince = computed(() => formatMonthYearFromUnix(props.authUser.registeredAt));
+
+const avatarPreviewImgDataUrl = computedAsync<string>(
+  async () => {
+    return singleFile.value
+      ? new Promise((resolve) => {
+          const reader = new FileReader();
+          reader.onload = ({ target: { result } }) => resolve(result.toString());
+          reader.readAsDataURL(singleFile.value);
+        })
+      : undefined;
+  },
+  undefined,
+  { lazy: true }
+);
+
+const avatarToShow = computed(
+  () => avatarPreviewImgDataUrl.value ?? props.authUser.avatar
 );
 
 const lasVisited = computed(() => diffFromUnix(props.authUser.lastVisitAt));
+
+watch(
+  singleFile,
+  (updatedVal) =>
+    updatedVal &&
+    emits("updateAvatarFile", {
+      id: props.authUser.id,
+      avatarFile: updatedVal,
+    })
+);
+
+watch(() => props.authUser.avatar, reset);
+
+function openDialog() {
+  open(fileDialogOptions);
+}
 </script>
 
 <template>
   <div class="profile-card">
-    <p class="text-center">
+    <div class="form-group" @click="openDialog">
       <img
-        :src="authUser.avatar"
+        :src="avatarToShow"
         class="avatar-xlarge"
         :title="`${authUser.name}'s profile picture`"
       />
-    </p>
+    </div>
 
     <h1 class="title" v-text="authUser.username" />
 
     <p class="text-lead" v-text="authUser.name" />
 
-    <p
-      class="text-justify"
-      v-text="!authUser.bio ? 'No bio specified' : authUser.bio"
-    />
+    <p class="text-justify" v-text="authUser.bio ?? 'No bio specified'" />
 
     <p>
       <fa style="color: #57ad8d" icon="circle-user" />&nbsp;
@@ -61,10 +101,7 @@ const lasVisited = computed(() => diffFromUnix(props.authUser.lastVisitAt));
 
     <p v-if="authUser.twitter" class="text-large text-center">
       <fa :icon="['fab', 'twitter']" />&nbsp;
-      <a
-        :href="`https://twitter.com/${authUser.twitter}`"
-        v-text="authUser.twitter"
-      />
+      <a :href="`https://twitter.com/${authUser.twitter}`" v-text="authUser.twitter" />
     </p>
 
     <p v-if="authUser.location" class="text-large text-center">

src/components/ProfileCardEditor.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
-import { reactive, toRaw, watch } from "vue";
+import { computedAsync, useFileDialog, UseFileDialogOptions } from "@vueuse/core";
+import { computed, reactive, watch } from "vue";
 import { UserVM } from "../models/UserVM";
 import { UserVmEditForInput } from "../types/userVm-types";
+
+const fileDialogOptions: UseFileDialogOptions = {
+  multiple: false,
+  accept: "image/*",
+};
 
 const props = defineProps<{
   user: UserVM;
@@ -13,20 +19,26 @@ const emits = defineEmits<{
   (e: "update:isDirty", state: boolean): void;
 }>();
 
-const { id, name, username, bio, email, website, location, avatar, twitter } =
-  toRaw(props.user);
+const { files, open } = useFileDialog();
 
-const userEditorObj = reactive<UserVmEditForInput>({
-  id,
-  name,
-  username,
-  bio,
-  email,
-  website,
-  location,
-  avatar,
-  twitter,
-});
+const userEditorObj = reactive<UserVmEditForInput>(props.user);
+
+const singleFile = computed(() => files.value?.item(0));
+
+const avatarPreviewImgDataUrl = computedAsync<string>(
+  async () =>
+    new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onload = ({ target: { result } }) => resolve(result.toString());
+      reader.readAsDataURL(singleFile.value);
+    }),
+  undefined,
+  { lazy: true }
+);
+
+const avatarToShow = computed(
+  () => avatarPreviewImgDataUrl.value ?? userEditorObj.avatar
+);
 
 watch(
   userEditorObj,
@@ -46,8 +58,13 @@ watch(
   }
 );
 
+function openDialog() {
+  open(fileDialogOptions);
+}
+
 function save() {
   emits("update:isDirty", false);
+  userEditorObj.avatarFile = singleFile.value;
   emits("save", userEditorObj);
 }
 
@@ -59,13 +76,13 @@ function cancel() {
 <template>
   <div class="profile-card">
     <form @submit.prevent="save">
-      <p class="text-center">
+      <div class="form-group" @click="openDialog">
         <img
-          :src="userEditorObj.avatar"
+          :src="avatarToShow"
           class="avatar-xlarge"
           :title="`${userEditorObj.name}'s profile picture`"
         />
-      </p>
+      </div>
 
       <div class="form-group">
         <input

src/pages/ProfilePage.vue
@@ -38,9 +38,7 @@ const { isReady } = useAsyncState(async () => {
 const hasDirtyForm = ref(false);
 
 const lastPostsDesc = computed(() =>
-  [...getAuthUser.value.posts].sort(
-    ({ publishedAt: a }, { publishedAt: b }) => b - a
-  )
+  [...getAuthUser.value.posts].sort(({ publishedAt: a }, { publishedAt: b }) => b - a)
 );
 
 const canReveal = computed(() => isReady.value && getAuthUser.value);
@@ -70,10 +68,7 @@ onBeforeRouteLeave(async () => {
 });
 
 function fetchUserPosts(): Promise<void> {
-  return postStore.fetchAllUserPosts(
-    pageSize,
-    getAuthUser.value.posts.at(-1)?.id
-  );
+  return postStore.fetchAllUserPosts(pageSize, getAuthUser.value.posts.at(-1)?.id);
 }
 
 async function goToHome() {
@@ -94,7 +89,11 @@ function cancel() {
 <template>
   <div v-if="canReveal" class="flex-grid">
     <div class="col-3 push-top">
-      <profile-card v-if="!edit" :auth-user="getAuthUser" />
+      <profile-card
+        v-if="!edit"
+        :auth-user="getAuthUser"
+        @update-avatar-file="userStore.updateAvatar"
+      />
       <profile-card-editor
         v-else
         v-model:is-dirty="hasDirtyForm"
@@ -114,10 +113,7 @@ function cancel() {
       <hr />
 
       <post-list :posts="lastPostsDesc" />
-      <app-infinite-scroll
-        :done="hasNoMorePosts"
-        @reached-end="fetchUserPosts"
-      />
+      <app-infinite-scroll :done="hasNoMorePosts" @reached-end="fetchUserPosts" />
       <!-- <div class="activity-list">
         <div class="activity">
           <div class="activity-header">

src/pages/RegisterPage.vue
@@ -121,7 +121,7 @@ commonStore.setReady();
           </button>
 
           <div v-if="avatarPreviewImgDataUrl" @click="openDialog">
-            <img :src="avatarPreviewImgDataUrl" class="avatar-xlarge" avatarPreviewImg />
+            <img :src="avatarPreviewImgDataUrl" class="avatar-xlarge" />
           </div>
         </div>
 

src/types/user-store-types.ts
@@ -2,6 +2,7 @@ import { ComputedRef, Ref } from "vue";
 import { UserVM } from "./../models/UserVM";
 import { StoreBaseActions, StoreBaseState } from "./store-base-types";
 import {
+  UserVMEditAvatarFile,
   UserVmEditForInput,
   UserVMNewFormInput,
   UserVMRegWithEmailAndPassword,
@@ -51,6 +52,11 @@ export interface UserStoreActions extends StoreBaseActions {
     fetchAfter?: boolean
   ): Promise<string>;
   editUser(dto: UserVmEditForInput, fetchAfter?: boolean): Promise<void>;
+  /**
+   * Uploads avatar file to Google Firebase Storage and returns URL of it.
+   * @returns URL of the uploaded avatar image file.
+   */
+  updateAvatar(dto: UserVMEditAvatarFile): Promise<string>;
   fetchUser(id: string): Promise<UserVM | undefined>;
   fetchUsers(ids?: Array<string>): Promise<Array<UserVM>>;
   /**

src/types/userVm-types.ts
@@ -14,10 +14,10 @@ export type UserVMFireBase = Omit<UserVM, "lastVisitAt" | "registeredAt"> & {
   registeredAt: Timestamp;
 };
 
-export type UserVMNewFormInput = Pick<
-  UserVM,
-  "name" | "username" | "email" | "avatar"
-> & { avatarFile?: File };
+export type UserVMAvatarFile = { avatarFile?: File };
+
+export type UserVMNewFormInput = UserVMAvatarFile &
+  Pick<UserVM, "name" | "username" | "email" | "avatar">;
 
 export type UserVmEditForInput = UserVMNewFormInput &
   Pick<UserVM, "id" | "bio" | "website" | "location" | "avatar" | "twitter">;
@@ -25,5 +25,7 @@ export type UserVmEditForInput = UserVMNewFormInput &
 export type UserVMRegWithEmailAndPassword = UserVMNewFormInput & {
   password: string;
 };
+
+export type UserVMEditAvatarFile = UserVMAvatarFile & Pick<UserVM, "id">;
 
 export const nameUser = nameofFactory<UserVM>();

commit 76090fa59428736080108e412d7281fc0b7e71f4

BUGFIX(user store): commit #2db07aca346efb126bd1b4cb4ce0c5a77ec25abf  introduced a bug:
- Better is trim the data before storing it to firestore.

src/stores/user-store.ts
@@ -179,14 +179,38 @@ export const useUserStore = defineStore(
     }
 
     async function editUser(
-      { id, avatarFile, ...rest }: UserVmEditForInput,
+      {
+        id,
+        avatarFile = undefined,
+        email = "",
+        name = "",
+        username = "",
+        avatar = null,
+        bio = null,
+        location = null,
+        twitter = null,
+        website = null,
+      }: UserVmEditForInput,
       fetchAfter = false
     ) {
-      avatarFile && (rest.avatar = await _uploadAvatar(avatarFile, id));
+      avatarFile && (avatar = await _uploadAvatar(avatarFile, id));
 
-      const editDto = Object.fromEntries(
-        Object.entries(rest).map(([k, v]) => [k, v === undefined ? null : v])
+      ok(
+        email && name && username,
+        "Edit user error: missing required filed values!"
       );
+
+      const editDto = {
+        id,
+        name,
+        email,
+        username,
+        avatar,
+        bio,
+        location,
+        twitter,
+        website,
+      };
 
       const userRef = _getRef(id);
 