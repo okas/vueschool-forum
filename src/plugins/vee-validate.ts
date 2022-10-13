4389cf25cb09c606ceda93094331ad4e157e7b2c
Lauri Saar <lauri.saar@outlook.com>
2022-10-13 22:25:56 +0100

    CHANGE(18.132): Customizing VeeValidate Error Messages

--- src/plugins/vee-validate.ts
+++ src/plugins/vee-validate.ts
@@ -1,8 +1,19 @@
+import { localize } from "@vee-validate/i18n";
 import { email, min, required } from "@vee-validate/rules";
-import { defineRule } from "vee-validate";
+import { configure, defineRule } from "vee-validate";
 
 export default () => {
   defineRule("required", required);
   defineRule("email", email);
   defineRule("min", min);
+
+  configure({
+    generateMessage: localize("en", {
+      messages: {
+        required: "{field} is required",
+        email: "{field} must be a valid email",
+        min: "{field} must be 0:{min} chars long",
+      },
+    }),
+  });
 };
