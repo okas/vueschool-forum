b745b473df1b4d002aa391311beb8b2b89f170d4
Lauri Saar <lauri.saar@outlook.com>
2022-10-20 12:13:41 +0100

    CHANGE(firestore data maintenance): upgraded scripts to support firestore admin API usage

--- firestore-fix-thread.js
+++ firestore-fix-thread.js
@@ -6,7 +6,7 @@ import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import serviceAccount from "./service-account.json" assert { type: "json" };
 
-const { log, error } = console;
+const { log, warn, error } = console;
 
 const app = initializeApp({
   credential: cert(serviceAccount),
@@ -18,31 +18,84 @@ log("- ➡️  transaction start");
 
 const threadsQuerySnap = await db.collection("threads").get();
 
+/**
+ * @param {FirebaseFirestore.Transaction} transaction
+ * @param {FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>} threadDocSnap
+ * @returns {Promise<void>[]}
+ */
+async function checkAndFixThreadDoc(transaction, threadDocSnap) {
+  {
+    const threadDocData = threadDocSnap.data();
+
+    /** @type { Array<string> | undefined } */
+    const posts = threadDocData?.posts;
+
+    if (!posts?.length) {
+      log(
+        `- ❕no posts: "${threadDocSnap.id}", fixes cannot be determined or might be unnecessary`
+      );
+
+      return;
+    }
+
+    if (!threadDocData.firstPostId) {
+      const firstPostId = posts[0];
+
+      const lastPostData = firstPostId || {
+        lastPostId: "",
+        lastPostAt: "",
+      };
+
+      const dto = {
+        firstPostId,
+        ...lastPostData,
+      };
+
+      transaction.update(threadDocSnap.ref, dto);
+
+      log(
+        `- ✔️  updated thread: "${threadDocSnap.id}", set "firstPostId": "${
+          dto.firstPostId
+        }"${lastPostData ?? ", resetted last post data."}`
+      );
+    }
+
+    if (!threadDocData.lastPostId || !threadDocData.lastPostAt) {
+      const lastPostId = posts.at(-1);
+      const lastPostDocSnap = await transaction.get(
+        db.collection("posts").doc(lastPostId)
+      );
+
+      if (!lastPostDocSnap.exists) {
+        warn(
+          `- ‼️  failed to set fix thread: "${threadDocSnap.id}", last post IF from posts prop do not exist in posts collection, skip"`
+        );
+
+        return;
+      }
+
+      const dto = {
+        lastPostId: lastPostDocSnap.id,
+        lastPostAt: lastPostDocSnap.data().publishedAt.seconds,
+      };
+
+      transaction.update(threadDocSnap.ref, dto);
+
+      log(
+        `- ✔️  updated thread: "${threadDocSnap.id}", set "lastPostId": "${dto.lastPostId}" & "lastPostAt": "${dto.lastPostAt}"`
+      );
+    }
+  }
+}
+
 await db
   .runTransaction(
     async (tran) => {
-      const threadTranUpdatePromises = threadsQuerySnap.docs.map(
-        (threadDocSnap) =>
-          new Promise((resolve) => {
-            const docData = threadDocSnap.data();
-
-            if (!docData.firstPostId) {
-              const dto = {
-                firstPostId: docData?.posts[0] ?? "",
-              };
-
-              tran.update(threadDocSnap.ref, dto);
-
-              log(
-                `- ✔️  updated thread: ${threadDocSnap.id} with fixed "firstPostId": "${dto.firstPostId}"`
-              );
-            }
-
-            resolve();
-          })
+      return Promise.all(
+        threadsQuerySnap.docs.map((threadDocSnap) =>
+          checkAndFixThreadDoc(tran, threadDocSnap)
+        )
       );
-
-      return Promise.all(threadTranUpdatePromises);
     },
     { maxAttempts: 1 }
   )
