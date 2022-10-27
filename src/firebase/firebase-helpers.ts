1fdbbb8f70efcfc7fb319b7366beab511ee1b5af
Lauri Saar <lauri.saar@outlook.com>
2022-10-27 12:59:44 +0100

    FIX(firebase to seconds conversion): added Date instance handling as well

--- src/firebase/firebase-helpers.ts
+++ src/firebase/firebase-helpers.ts
@@ -1,9 +1,11 @@
 import { Timestamp } from "firebase/firestore";
 
 export function tryGetSeconds<
-  TInput extends Timestamp | undefined | null | number
+  TInput extends Date | Timestamp | undefined | null | number
 >(fbTimeStampOrAsIs: TInput): number {
   return fbTimeStampOrAsIs instanceof Timestamp
     ? fbTimeStampOrAsIs.seconds
+    : fbTimeStampOrAsIs instanceof Date
+    ? Math.floor(fbTimeStampOrAsIs.getTime() / 1000)
     : Number(fbTimeStampOrAsIs);
 }
