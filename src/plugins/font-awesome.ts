077d1db6cd42d7641d6a10865aa089100b09c4ae
Lauri Saar <lauri.saar@outlook.com>
2022-10-27 13:33:36 +0100

    FIX(font awesome plugin): tree-shaking was not working

--- src/plugins/font-awesome.ts
+++ src/plugins/font-awesome.ts
@@ -1,50 +1,44 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import {
-  library,
-  type IconDefinition,
-} from "@fortawesome/fontawesome-svg-core";
+  faCamera,
+  faCircleUser,
+  faComment,
+  faComments,
+  faGlobe,
+  faImagePortrait,
+  faLocationDot,
+  faPaperPlane,
+  faPencilAlt,
+  faSpinner,
+  faUser,
+  faUserCircle,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import type { App } from "vue";
 
 export async function useFontAwesomePlugin() {
-  const appIconDefinitions = await getIconDefinitions();
-
   return {
     install(app: App<Element>, componentName?: string) {
-      library.add(...appIconDefinitions);
+      library.add(
+        faPencilAlt,
+        faSpinner,
+        faLocationDot,
+        faGlobe,
+        faCircleUser,
+        faPaperPlane,
+        faXmark,
+        faCamera,
+        faImagePortrait,
+        faUserCircle,
+        faUser,
+        faComments,
+        faComment,
+        faTwitter
+      );
 
       app.component(componentName ?? "Fa", FontAwesomeIcon);
     },
   };
-}
-
-async function getIconDefinitions(): Promise<IconDefinition[]> {
-  // If there are icons form other FA module, then add dynamic import statement
-  // with icon definitions array as a return
-  const freeSolid = await import("@fortawesome/free-solid-svg-icons").then(
-    (m) => {
-      return [
-        m.faPencilAlt,
-        m.faSpinner,
-        m.faLocationDot,
-        m.faGlobe,
-        m.faCircleUser,
-        m.faPaperPlane,
-        m.faXmark,
-        m.faCamera,
-        m.faImagePortrait,
-        m.faUserCircle,
-        m.faUser,
-        m.faComments,
-        m.faComment,
-      ];
-    }
-  );
-
-  const freeBrand = await import("@fortawesome/free-brands-svg-icons").then(
-    (m) => {
-      return [m.faTwitter];
-    }
-  );
-
-  return [...freeSolid, ...freeBrand];
 }
